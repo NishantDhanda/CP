be284648979154bbc10a73efff1be7a95518987a
NishantDhanda <54473864+NishantDhanda@users.noreply.github.com>
2019-08-25 09:26:05 +0530

    Create STL_7.cpp

---
+++ STL_7.cpp
@@ -0,0 +1,12 @@
+#include<cstring>
+#include<iostream>
+using namespace std;
+int main()
+{
+    // array of strings
+    string s[]={"lol","Ha ha ha","1 2 345","he he he"};
+    for(int i=0;i<4;i++)
+    {
+        cout<<s[i]<<endl;
+    }
+}
